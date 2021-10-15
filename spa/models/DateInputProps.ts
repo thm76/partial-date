f6e15666969e74422ad5e94e0b925c2a4ad9a55e
Thomas Müller <thomas@lateniteapps.com>
2021-10-15 15:02:55 +1100

    Remove some unnecessary files

--- spa/models/DateInputProps.ts
+++ spa/models/DateInputProps.ts
@@ -3,7 +3,6 @@ import { PartialDate } from "./PartialDate";
 
 export type DateInputProps = {
   date: Accessor<PartialDate>;
-  // date: Maybe<PartialDate>;
   setDate: Setter<PartialDate>;
   allowsPartial?: boolean;
 };
